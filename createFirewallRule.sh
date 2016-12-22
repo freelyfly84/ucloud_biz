4063d42704f818e164f226f6c8e8ff764f7145b1
freelyfly84 <freelyfly84@gmail.com>
2016-12-22 16:30:58 +0900

    Create createFirewallRule.sh

---
+++ createFirewallRule.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apikey=`cat ~/.cloudmonkey/config | grep apikey | head -1 | awk '{print $3}'`
+secretkey=`cat ~/.cloudmonkey/config | grep secretkey | head -1 | awk '{print $3}'`
+
+cloudmonkey set apikey $apikey
+cloudmonkey set secretkey $secretkey
+cloudmonkey set display csv
+
+IFS=,
+sed 1d $1 | while read cidrlist protocol start end ip
+ do
+        ipid=`python checkIpid.py $apikey $secretkey $ip`
+        cloudmonkey createFirewallRule ipaddressid=$ipid protocol=$protocol cidrlist=$cidrlist startport=$start endport=$end
+ done
