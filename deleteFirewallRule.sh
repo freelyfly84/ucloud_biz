a9694e207546b1e6a2713027f53c771c32a6e414
freelyfly84 <freelyfly84@gmail.com>
2016-12-23 11:02:10 +0900

    Update deleteFirewallRule.sh

--- deleteFirewallRule.sh
+++ deleteFirewallRule.sh
@@ -8,10 +8,9 @@ cloudmonkey set secretkey $secretkey
 cloudmonkey set display csv
 
 IFS=,
-sed 1d $1 | while read cidrlist protocol start end ip
+sed 1d $1 | while read start end cidrlist protocol ip
  do
         ipid=`python checkIpid.py $apikey $secretkey $ip`
-        fwid=`cloudmonkey listFirewallRules ipaddressid=$ipid filter=cidrlist,id,startport,protocol | grep $cidrlist | grep $start | grep $protocol | awk -F , '{print $4}'`
-
+        fwid=`cloudmonkey listFirewallRules ipaddressid=$ipid filter=cidrlist,id,startport,endport,protocol | grep -w $cidrlist | grep -w $start | grep -w $end | grep -w $protocol | awk -F , '{print $5}'`
         cloudmonkey deleteFirewallRule id=$fwid
  done
