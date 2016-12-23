db062143b37017c336a29fcf790d71f74c9f85c4
freelyfly84 <freelyfly84@gmail.com>
2016-12-23 11:01:02 +0900

    Update createPortForwardingRule.sh

--- createPortForwardingRule.sh
+++ createPortForwardingRule.sh
@@ -10,9 +10,7 @@ cloudmonkey set display csv
 IFS=,
 sed 1d $1 | while read vm protocol public private ip
  do
-	ipid=`python checkIpid.py $apikey $secretkey $ip`
-	vmid=`cloudmonkey listVirtualMachines filter=id,name,displayname | grep $vm | awk -F , '{print $1}'`
-        cloudmonkey createPortForwardingRule ipaddressid=$ipid virtualmachineid=$vmid protocol=$protocol publicport=$public privateport=$private 
- done 
-
-
+        ipid=`python checkIpid.py $apikey $secretkey $ip`
+        vmid=`cloudmonkey listVirtualMachines filter=id,name,displayname | grep -w $vm | awk -F , '{print $1}'`
+        cloudmonkey createPortForwardingRule ipaddressid=$ipid virtualmachineid=$vmid protocol=$protocol publicport=$public privateport=$private
+ done
